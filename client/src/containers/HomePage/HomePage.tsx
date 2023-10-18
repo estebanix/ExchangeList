c8d52d97a7b955de0db1f4a8d861a69936514007
estebanix <dakisunshine@gmail.com>
2023-10-18 12:35:50 +0200

    fetching api

--- client/src/containers/HomePage/HomePage.tsx
+++ client/src/containers/HomePage/HomePage.tsx
@@ -1,5 +1,39 @@
+import React, { useEffect, useState } from "react";
 import styles from "./HomePage.module.scss";
 
-export const HomePage = () => {
-  return <div className={styles.homePageContainer}>asdF</div>;
+interface RenderProps {
+  shortName: string;
+}
+
+export const HomePage: React.FC = () => {
+  const [state, setState] = useState<RenderProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e"
+        );
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+
+        const data = await response.json();
+        setState(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.homePageContainer}>
+      {state.map((dat, index) => (
+        <p key={index}>{dat.shortName}</p>
+      ))}
+    </div>
+  );
 };
